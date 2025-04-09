ceffe377ec9c0ae153980e7845f592bd21c6eeae
Emil Bragstad <emilbrag@ntnu.no>
2025-04-09 12:42:05 +0200

    Automate startup w/ script

--- docker/start_test-3-drones-with-commando.sh
+++ docker/start_test-3-drones-with-commando.sh
@@ -11,8 +11,14 @@ cargo build
 docker-compose down
 docker-compose up -d
 docker cp $HOME/valkyrie-mls/target/debug/valkyrie-mls node2:/valkyrie-mls/valkyrie-mls
+docker cp $HOME/valkyrie-mls/src/config.toml node2:/valkyrie-mls/config.toml
+
+
 docker cp $HOME/valkyrie-mls/target/debug/valkyrie-mls node3:/valkyrie-mls/valkyrie-mls
+docker cp $HOME/valkyrie-mls/src/config.toml node3:/valkyrie-mls/config.toml
+
 docker cp $HOME/valkyrie-mls/target/debug/valkyrie-mls node4:/valkyrie-mls/valkyrie-mls
+docker cp $HOME/valkyrie-mls/src/config.toml node4:/valkyrie-mls/config.toml
 
 # Wait a moment to let containers start up
 sleep 2
