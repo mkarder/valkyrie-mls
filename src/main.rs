c9fa40dacf9936db2a1eddb36352e840108dc772
Lab <emilbrag@stud.ntnu.no>
2025-04-22 13:08:19 +0200

    Change reading from file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 pub mod authentication;
 mod config;
 pub mod mls_group_handler;
-use anyhow::{Ok, Result};
+use anyhow::Result;
 
 #[cfg(target_os = "linux")]
 mod corosync;
@@ -12,6 +12,7 @@ use mls_group_handler::MlsEngine;
 #[cfg(target_os = "linux")]
 use router::Router;
 use std::env;
+use std::path::PathBuf;
 
 
 #[tokio::main]
@@ -21,11 +22,23 @@ async fn main() -> Result<()> {
     #[cfg(target_os = "linux")]
     {
         log::info!("Starting MLS Valkyrie...");
-        let config_path = format!("{}/valkyrie-mls/config.toml", env::var("HOME").unwrap());
-        let config = Config::from_file(config_path.as_str()).unwrap();
 
+        // Determine correct home directory
+        let user_home = match env::var("SUDO_USER") {
+            Ok(user) => PathBuf::from(format!("/home/{}", user)),
+            Err(_) => home::home_dir().expect("Cannot determine home directory"),
+        };
+
+        // Construct full config path
+        let config_path = user_home.join("valkyrie-mls").join("config.toml");
+
+        log::info!("Using config path: {}", config_path.display());
+
+        // Read config
+        let config = Config::from_file(config_path.to_str().unwrap()).unwrap();
+
+        // Start MLS system
         let mls_engine = MlsEngine::new(config.mls.clone());
-
         let mut router = Router::new(mls_engine, config.router.clone());
         router.run_main_loop().await?;
 
